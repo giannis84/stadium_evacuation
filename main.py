792abeb3e9852d4fb68e5e65d8e4ca8b5890886e
g_kos <giannis@student.chalmers.se>
2020-11-18 14:57:46 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# SIMULATIONS OF COMPLEX SYSTEMS
+# 2020 Project: Simulation of stadium evacuation
+# Collaborators: Alfred Bergsten, Giannis Kostaras, Mohammad Zoubi, Andreas Spetz
+
+def print_info(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_info('Stadium evacuation simulation.')
